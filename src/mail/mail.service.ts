6422d92433c76f216b9bcbb16ee6b6818b9fd6bd
evelez6082 <josue.vlez@gmail.com>
2026-05-11 03:39:14 -0500

    Agregue requireTLS: true

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -15,7 +15,8 @@ export class MailService {
   private transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: Number(process.env.EMAIL_PORT),
-    secure: process.env.EMAIL_SECURE === 'true',
+    secure: false,
+    requireTLS: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
